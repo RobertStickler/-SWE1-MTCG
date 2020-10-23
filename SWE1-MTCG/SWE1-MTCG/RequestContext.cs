5ce1001ff9480ffb10dc026b7a811e3faa083444
Robert Stickler <if19b098@technikum-wien.at>
2020-10-23 09:33:10 +0200

    initial

---
+++ SWE1-MTCG/SWE1-MTCG/RequestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWE1_MTCG
+{
+    public class RequestContext
+    {
+
+
+        string method;
+        string header;
+        string version;
+        string path;
+
+
+
+    }
+}
