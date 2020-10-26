bf98e00914a664d2567e9706854403f42a16da93
Robert Stickler <if19b098@technikum-wien.at>
2020-10-26 13:01:47 +0100

    git merge

--- SWE1-MTCG/SWE1-MTCG/Cards/BaseCard.cs
+++ SWE1-MTCG/SWE1-MTCG/Cards/BaseCard.cs
@@ -5,49 +5,49 @@ using Enum;
 
 namespace Cards
 {
-	public abstract class BaseCards
-	{
+    public abstract class BaseCards
+    {
 
-		protected elementTypes element_type;
-		protected cardTypes card_type;
-		protected cardProperty card_property;
+        protected elementTypes element_type;
+        protected cardTypes card_type;
+        protected cardProperty card_property;
 
-		protected int card_damage;
-		protected string card_name;
-		public BaseCards(int damage, string name)
-		{
-			this.card_damage = damage;
-			this.card_name = name;
-		}
-
-		public BaseCards(int damage, string name, elementTypes element)
+        protected int card_damage;
+        protected string card_name;
+        public BaseCards(int damage, string name)
         {
-			this.card_damage = damage;
-			this.card_name = name;
-			this.element_type = element; 
-		}
-
-
-
-
-
-		public int getCardDamage()
-		{
-			return card_damage;
-		}
-		public string getCardName()
-		{
-			return card_name;
-		}
-		public cardTypes getCardType()
-        {			
-			return card_type;
+            this.card_damage = damage;
+            this.card_name = name;
         }
-		public elementTypes getElementTypes()
+
+        public BaseCards(int damage, string name, elementTypes element)
         {
-			return element_type;
+            this.card_damage = damage;
+            this.card_name = name;
+            this.element_type = element;
         }
-	}
+
+
+
+
+
+        public int getCardDamage()
+        {
+            return card_damage;
+        }
+        public string getCardName()
+        {
+            return card_name;
+        }
+        public cardTypes getCardType()
+        {
+            return card_type;
+        }
+        public elementTypes getElementTypes()
+        {
+            return element_type;
+        }
+    }
 }
 
 
