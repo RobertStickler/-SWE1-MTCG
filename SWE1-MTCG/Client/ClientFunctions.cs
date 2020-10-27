b0c040f9ea1fce23ae41860ca1b9e75497091dc5
Robert Stickler <rstickler87@gmail.com>
2020-10-27 19:02:02 +0100

    HTTPClient added

---
+++ SWE1-MTCG/Client/ClientFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class ClientFunctions
+    {
+        static readonly HttpClient client = new HttpClient();
+        public void GetRequest(string ur)
+        {
+
+        }
+
+        public async Task PostRequest(string uri)
+        {
+            try
+            {
+                Console.WriteLine("enter your message");
+                string message = Console.ReadLine();
+                HttpContent data = new StringContent(message, Encoding.UTF8, "text/plain");
+
+                HttpResponseMessage postReturn = await client.PostAsync(uri, data);
+                string result = postReturn.Content.ReadAsStringAsync().Result;
+                var send = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, uri));
+
+                Console.WriteLine(postReturn);
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                throw new HttpRequestException();
+            }
+            
+        }
+
+
+
+    }
+}
--- SWE1-MTCG/Client/Program.cs
+++ SWE1-MTCG/Client/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+        
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            string port = "6543";
+            string ipAdress = "127.0.0.1";
+            string advice = "/messages";
+            string messageNumber = "/1";
+
+            string uri = "http://" + ipAdress + ":" + port + advice; //das von insomnia hald
+
+            Console.WriteLine("1...GET");
+            Console.WriteLine("2...POST");
+            Console.WriteLine("3...PUT");
+            Console.WriteLine("4...DELETE");
+
+            ClientFunctions clientFunc = new ClientFunctions();
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice >= 5)
+                {
+                    Console.WriteLine("error");
+                }
+                else if(choice < 1)
+                {
+                    Console.WriteLine("error");
+                }
+
+                switch(choice)
+                {
+                    case 1: //Get
+                        {
+                            clientFunc.GetRequest(uri);
+                            break;
+                        }
+                    case 2: //Post
+                        {
+                            clientFunc.PostRequest(uri);
+                            break;
+                        }
+                }
+                
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            Console.Read();
         }
     }
 }
+
