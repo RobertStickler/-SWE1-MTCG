7b2328ac7b4367cbd5a430138b3142b4aa2f7751
Robert Stickler <rstickler87@gmail.com>
2020-11-03 22:43:22 +0100

    TCPClass deleted, small fixes

--- SWE1-MTCG/Server/Program.cs
+++ SWE1-MTCG/Server/Program.cs
@@ -1,5 +1,4 @@
-﻿using Server;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -8,14 +7,46 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
-class SERVER
+class Server
 {
-    
     static void Main(string[] args)
     {
-        MyTcpListener server = new MyTcpListener();
-        server.startServer();
+        Server.startServer();
     }
+
+    static void receiveData(TcpClient client, NetworkStream stream)
+    {
+        byte[] bytes = new byte[client.ReceiveBufferSize];
+        stream.Read(bytes, 0, (int)client.ReceiveBufferSize);
+        string returndata = Encoding.UTF8.GetString(bytes);
+        Console.WriteLine("SERVER RECEIVED:\n" + returndata);
+    }
+
+    static void startServer()
+    {
+        new Thread(() =>
+        {
+            Console.Write("Waiting for a connection... ");
+            TcpListener listener = new TcpListener(IPAddress.Any, 6543);
+            listener.Start();
+
+            while (true)
+            {
+                if (listener.Pending())
+                {
+                    new Thread(() =>
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
+                        NetworkStream stream = client.GetStream();
+                        Server.receiveData(client, stream);
+                    }).Start();
+                }
+            }
+        }).Start();
+    }
+
+
 }
 
 
