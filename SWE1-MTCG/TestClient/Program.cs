f5b704451204f450dbb923a57715a7232c4f4c44
Robert Stickler <rstickler87@gmail.com>
2020-11-04 18:19:16 +0100

    Multithread Server with response message

---
+++ SWE1-MTCG/TestClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6543;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+            Socket clientSock = new Socket
+            (
+               AddressFamily.InterNetwork,     
+               SocketType.Stream,              
+                                               
+               ProtocolType.Tcp
+            );
+
+            clientSock.Connect(new IPEndPoint(ip, port));
+            Console.WriteLine("Client connected!");
+
+            while(true)
+            {
+                Console.WriteLine("Enter message:");
+                string msgClient = Console.ReadLine();
+
+                byte[] msgClientByte = new byte[1024];
+                //msg = Encoding.ASCII.GetString(messageClient);
+                msgClientByte = Encoding.ASCII.GetBytes(msgClient);
+
+                //clientSock.Send(System.Text.Encoding.ASCII.GetBytes(msgClient), 0, msgClient.Length, SocketFlags.None);
+                clientSock.Send(msgClientByte, 0, msgClientByte.Length, SocketFlags.None);
+
+                byte[] msgServerByte = new byte[1024];
+                int size = clientSock.Receive(msgServerByte);
+                string msgServer = Encoding.ASCII.GetString(msgServerByte, 0, size);
+                Console.WriteLine("Server response:\n" + msgServer + "\n");
+                //Console.WriteLine("Server:\n", System.Text.Encoding.ASCII.GetString(msgServerByte,0,size));
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ SWE1-MTCG/TestServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6543;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+            Socket serverSock = new Socket
+            (
+                AddressFamily.InterNetwork,     // IPv4
+                SocketType.Stream,              // Stream sends byte array based data
+                                                // and supports reliable 2way communication
+                ProtocolType.Tcp                // TCP
+            );              
+
+
+            //IPEndPoint endPoint = new IPEndPoint(ip, port);
+            serverSock.Bind(new IPEndPoint(ip, port));
+            serverSock.Listen(5);
+
+            Console.WriteLine("Server is listening.....\n");
+
+            Socket clientSock = default(Socket);
+            int counter = 0;
+            Program p = new Program();
+
+            while(true)
+            {
+                counter++;
+                clientSock = serverSock.Accept();
+                Console.WriteLine(counter + " Clients connected");
+                Thread UserThread = new Thread(new ThreadStart(() => p.User(clientSock)));
+                UserThread.Start();
+            }
+
+           
+        }
+        public void User(Socket client)
+        {
+            while(true)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                client.Send(msg, 0, size, SocketFlags.None);
+            }
+        }
+    }
+}
